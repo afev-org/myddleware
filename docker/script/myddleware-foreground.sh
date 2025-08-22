3b96e26374c646078c9ecdd229570e27c621c402
AlexMyddleware <alexistroitzky@myddleware.com>
2025-08-22 13:43:31 +0200

    restore chowns

--- docker/script/myddleware-foreground.sh
+++ docker/script/myddleware-foreground.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env bash
 
+mkdir -p var/cache var/log
+chmod -R 700 var/cache
+chown -R www-data:www-data var/cache
+chmod -R 700 var/log
+chown -R www-data:www-data var/log
+
 ## Extend Hosts
 echo "====[ UPDATE HOSTS ]===="
 cat hosts >> /etc/hosts
 cat /etc/hosts
 echo "--"
-
-## Start Cronjob
-echo "====[ PREPARE CRON ]===="
-printenv | sed "s/^\(.*\)$/export \\1/g" | grep -E "^export MYSQL_" > /run/crond.env
-cat crontab.client >> /etc/crontab
-cat /etc/crontab
-echo "--"
-rsyslogd
-cron
-
-## Start yarn build
-composer dump-autoload --optimize
-yarn install
-yarn run build
 
 ## Start Apache
 echo "====[ START APACHE ]===="
